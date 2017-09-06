257f291a650a5b372ff60441773e470c9b782cbc
byenogkwan <byeongkwan@whatshoe.co.kr>
2017-09-06 17:48:09 +0900

    printf byeongkwan study add

--- report_card.c
+++ report_card.c
@@ -9,9 +9,10 @@ int main()
 	eng = 100;
 	kor = 90;
 	sum = 80 + 100 + 90;
-
+        
+	printf("byeongkwan study");
 	printf("This program print report card.\n");
-
+	printf(“test”);
 	printf("Korean : %d\n", kor);
 	printf("English : %d\n", eng);
 	printf("Math : %d\n", math);
